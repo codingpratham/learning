2a0eff256c865b0a3caf81cefda79a0465f75b11
codingpratham <coding.job17@gmail.com>
2024-09-01 23:03:07 +0530

    learn about axios use effect useMemo

--- week-6/hooks/src/App.jsx
+++ week-6/hooks/src/App.jsx
@@ -1,17 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import Todo from './component/Todo';
+import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
-  
-  return (
-    <>
-      
-        <Todo
-          id={2}
-        />
-      
-    </>
-  );
-};
+  const [counter,setCounter]=useState(0)
+  const [input,setInput]=useState(0)
 
-export default App;
+  let count=0
+
+  for(let i=0;i<=input;i++){
+    count+=i
+  }
+
+  return (
+    <div>
+
+      <input type="text"value={input}
+      onChange={(e)=>{setInput(e.target.value)}} />
+
+      <h1>sum from 1 to {input} is {count}</h1>
+
+      <button onClick={()=>{
+        setCounter(counter+1)
+      }}>Counter({counter})</button>
+    </div>
+  )
+}
+
+export default App
--- week-6/hooks/src/component/Todo.jsx
+++ week-6/hooks/src/component/Todo.jsx
@@ -1,21 +1,29 @@
 import axios from 'axios';
 import React from 'react'
+
+
 const Todo = ({
     id
 }) => {
-    const [todos, setTodos] = React.useState([]);
+    const [todo, setTodo] = React.useState({});
 
  React.useEffect(() => {
    axios.get('http://localhost:3000/todo?id='+id)
    .then((res)=>{
-    setTodos(res.data.todos)
+    setTodo(res.data.todo)
    })
-  }, []);
+   .catch((err)=>{
+    console.error(err);
+    
+   })
+  }, [id]);
 
+ 
   return (
     <div>
-        <h1>{title}</h1>
-        <h3>{description}</h3>
+        <h1>{todo.title}</h1>
+        <h3>{todo.description}</h3>
+        
     </div>
   )
 }
