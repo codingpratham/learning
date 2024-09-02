d52e95b241b37c0f3f9bf5162207e7b8bb7b126b
codingpratham <coding.job17@gmail.com>
2024-09-02 22:48:33 +0530

    today i learned about useCallback

--- week-6/hooks/src/App.jsx
+++ week-6/hooks/src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import { useEffect } from 'react'
 import { useState } from 'react'
 
 const App = () => {
   const [counter,setCounter]=useState(0)
   const [input,setInput]=useState(0)
 
-  let count=0
+  useEffect(()=>{
 
-  for(let i=0;i<=input;i++){
-    count+=i
-  }
+    let count=0
+  
+    for(let i=0;i<=input;i++){
+      count+=i
+    }
+
+    setInput(count)
+  },[input])
 
   return (
     <div>
@@ -17,7 +23,7 @@ const App = () => {
       <input type="text"value={input}
       onChange={(e)=>{setInput(e.target.value)}} />
 
-      <h1>sum from 1 to {input} is {count}</h1>
+      <h1>sum from 1 to {input} is {setInput}</h1>
 
       <button onClick={()=>{
         setCounter(counter+1)
