0719dee0a3566cefbcc19e729992af8d5240d110
codingpratham <coding.job17@gmail.com>
2024-09-04 21:55:04 +0530

    learned about routers

--- week-7/Routers-context-Api/src/App.jsx
+++ week-7/Routers-context-Api/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
+const Dashboard = lazy(() => import('./Component/Pages/Dashboard'));
+import Landing from './Component/Pages/Landing';
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <BrowserRouter>
+        <AppBar />
+        <Routes>
+          <Route path='/dashboard' element={
+            <Suspense>
+
+              <Dashboard />
+            </Suspense>
+            } />
+          <Route path='/landing' element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+function AppBar() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <button
+        onClick={() => {
+          navigate('/landing');
+        }}
+      >
+        Landing
+      </button>
+      <button
+        onClick={() => {
+          navigate('/dashboard');
+        }}
+      >
+        Dashboard
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
