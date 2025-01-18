c6709f9ce66cecbce0d7e6ea4528cdaede77502a
codingpratham <coding.job17@gmail.com>
2025-01-18 17:44:16 +0530

    .

--- week-16/week-16-1/apps/backend/src/index.ts
+++ week-16/week-16-1/apps/backend/src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express'
-
+import {BACKEND_URL} from '@repo/common/config'
 const app = express()
 
 app.get('/',(req,res)=>{
     res.send('Hello from Express!')
+    res.send(BACKEND_URL)
 })
 
 app.listen('3002')
