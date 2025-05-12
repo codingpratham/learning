0840cf986f567cea9e5bd70b74bc69deca957033
codingpratham <coding.job17@gmail.com>
2025-05-12 15:18:18 +0530

    .

---
+++ week-19-offline/ws-client/src/App.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import './App.css'
+
+const App = () => {
+  const [socket,setSocket]=useState<WebSocket| null>(null)
+  const [message , setMessage]=useState("")
+  const [Lmessage , setLMessage]=useState("")
+
+  useEffect(()=>{
+    const socket =  new WebSocket('http://localhost:3000')
+
+    socket.onopen=()=>{
+      console.log("Connection established");
+      setSocket(socket)
+    }
+    socket.onmessage=(message)=>{
+      console.log("message recieved:",message.data);
+      setMessage(message.data)
+    }
+
+    setSocket(socket)
+  },[])
+  return (
+    <>
+    <input type="text" value={Lmessage} onChange={e=>{
+      setLMessage(e.target.value)
+    }}/>
+
+    <button onClick={()=>{
+      socket?.send(Lmessage)
+      setLMessage(Lmessage)
+      
+    }}>Send</button>
+    {message}
+    </>
+  )
+}
+
+export default App
